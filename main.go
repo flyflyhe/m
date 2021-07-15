8a12386727d8270534145eb65ab7b6c889544872
满仓 <hejinxue001@lonlife.cn>
2021-07-15 09:59:55 +0800

    接口型函数

--- main.go
+++ main.go
@@ -6,11 +6,13 @@ type Handler interface {
 	Do(k, v interface{})
 }
 
+// HandlerFunc 接口型函数
 type HandlerFunc func(k, v interface{})
 
 func (f HandlerFunc) Do(k, v interface{}) {
 	f(k, v)
 }
+
 
 func Each(m map[interface{}]interface{}, h Handler) {
 	if m != nil && len(m) > 0 {
@@ -24,6 +26,14 @@ func EachFunc(m map[interface{}]interface{}, f func(k, v interface{})) {
 	Each(m, HandlerFunc(f))
 }
 
+
+type welcome string
+
+// Do 如果不用接口型函数 函数名就不能表达确切的含义
+func (w welcome) Do(k, v interface{}) {
+	fmt.Printf("%s,我叫%s,今年%d岁\n", w,k, v)
+}
+
 func selfInfo(k, v interface{}) {
 	fmt.Printf("大家好,我叫%s,今年%d岁\n", k, v)
 }
@@ -34,5 +44,7 @@ func main() {
 	persons["李四"] = 23
 	persons["王五"] = 26
 
-	EachFunc(persons, selfInfo)
+	var w welcome = "大家好"
+
+	Each(persons, w)
 }
