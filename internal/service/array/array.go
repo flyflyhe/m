df569a0e3f61a0f7be9658c45fd29678896a6b69
满仓 <hejinxue001@lonlife.cn>
2021-09-13 15:25:53 +0800

    二分

---
+++ internal/service/array/array.go
@@ -0,0 +1,29 @@
+package array
+
+func peakIndexInMountainArray(arr []int) int {
+	point := 0
+	for i := 1; i < len(arr); i++ {
+		if arr[i-1] < arr[i] {
+			point = i
+		}
+	}
+
+	return point
+}
+
+
+func peakIndexInMountainArray2(arr []int) int {
+	left := 1
+	right := len(arr) - 1
+
+	for left < right {
+		mid := left + right + 1 >> 1
+		if arr[mid - 1] < arr[mid]  {
+			left = mid
+		} else {
+			right = mid + 1
+		}
+	}
+
+	return right
+}
