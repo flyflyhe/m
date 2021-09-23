a6d1794281e06b29d3795c78693d43994e1169d0
满仓 <hejinxue001@lonlife.cn>
2021-09-23 15:57:29 +0800

    合并两个有序数组

--- internal/service/array/array.go
+++ internal/service/array/array.go
@@ -67,3 +67,27 @@ func findMaxAverage(nums []int, k int) float64 {
 
 	return ans
 }
+
+func mergeSortArr(nums1 []int, m int, nums2 []int, n int)  {
+	i := m - 1
+	j := n - 1
+	idx := m + n - 1
+	for i >= 0 || j >= 0 {
+		if i >= 0 && j >= 0 {
+			if nums1[i] > nums2[j] {
+				nums1[idx] = nums1[i]
+				i--
+			} else {
+				nums1[idx] = nums2[j]
+				j--
+			}
+		} else if i >= 0 {
+			nums1[idx] = nums1[i]
+			i--
+		} else {
+			nums1[idx] = nums2[j]
+			j--
+		}
+		idx--
+	}
+}
