d737312a380e0ac8979109544a808513f74e5790
满仓 <hejinxue001@lonlife.cn>
2021-10-08 09:17:49 +0800

    magicalNumber

--- internal/service/num/num.go
+++ internal/service/num/num.go
@@ -1,5 +1,7 @@
 package num
 
+import "math"
+
 func computeArea(ax1, ay1, ax2, ay2, bx1, by1, bx2, by2 int) int {
 	area1 := (ax2 - ax1) * (ay2 - ay1)
 	area2 := (bx2 - bx1) * (by2 - by1)
@@ -21,4 +23,19 @@ func max(a, b int) int {
 		return b
 	}
 	return a
+}
+
+func nthMagicalNumber(n int, a int, b int) int {
+	var r uint64
+	r = 1
+	max := uint64(2 << 63)
+	for i := 1; i <= n; i++ {
+		for j := r; j < max; j++ {
+			if j % uint64(a) == 0 || j % uint64(b) == 0 {
+				r = j
+			}
+		}
+	}
+
+	return int(r % uint64(math.Pow10(9)) + 7)
 }
