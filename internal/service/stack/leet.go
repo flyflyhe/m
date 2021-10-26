d4b69cb07e71ced570bda3987721f510474c5163
满仓 <hejinxue001@lonlife.cn>
2021-10-26 17:30:46 +0800

    单调栈 求温度

--- internal/service/stack/leet.go
+++ internal/service/stack/leet.go
@@ -17,3 +17,35 @@ func ValidateStackSequences(pushed []int, popped []int) bool {
 
 	return stack.Len() == 0
 }
+
+/**
+使用单调栈
+[73,74,75,71,69,72,76,73]
+ */
+
+func dailyTemperatures(temperatures []int) []int {
+	length := len(temperatures)
+	if length == 0 {
+		return nil
+	}
+	ret := make([]int, length)
+	stack := make([]int, 1)
+	stack[0] = 0
+	for i := 1; i < length; i++ {
+		stackIndex := stack[len(stack) - 1]
+		if temperatures[i] <= temperatures[stackIndex] {
+			stack = append(stack, i)
+		} else {
+			for temperatures[i] > temperatures[stackIndex] { //新入栈大于栈顶
+				ret[stackIndex] = i - stackIndex
+				stack = stack[:len(stack) - 1]
+				if len(stack) == 0 {
+					break
+				}
+				stackIndex = stack[len(stack) - 1]
+			}
+			stack = append(stack, i)
+		}
+	}
+	return ret
+}
