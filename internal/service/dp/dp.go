276a33e1f5861702b8a82c7ee2d9539e4fa640bb
mrHe <hejinxue001@163.com>
2021-11-02 07:31:54 +0800

    dp cost

--- internal/service/dp/dp.go
+++ internal/service/dp/dp.go
@@ -56,3 +56,20 @@ func fibDp(n int) int {
 
 	return arr[n % 3]
 }
+
+func minCostClimbingStairs(cost []int) int {
+	length := len(cost)
+	dp := make([]int, length + 1)
+	dp[0] = 0
+	dp[1] = 0
+
+	for i := 2; i <= length; i++ {
+		if dp[i-1] + cost[i-1] > dp[i-2] + cost[i-2] {
+			dp[i] = dp[i-2] + cost[i-2]
+		} else {
+			dp[i] = dp[i-1] + cost[i-1]
+		}
+	}
+
+	return dp[length]
+}
