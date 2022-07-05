fd3b6b341f1a811983fac664d19026ebf3f6d785
满仓 <hejinxue001@lonlife.cn>
2022-07-05 14:22:00 +0800

    treeSet 方式修改

--- internal/service/tree/treeSet.go
+++ internal/service/tree/treeSet.go
@@ -28,25 +28,22 @@ func (set *ArraySet) Set(Key [2]int, val interface{}) bool {
 			c = set.Comparator(Key, node.Key)
 			if c == 0 {
 				return false
-			} else {
-				if c == -1 {
-					if node.Left != nil {
-						node = node.Left
-					} else {
-						node.Left = &SetNode{Key: Key, Prev: node}
-						return true
-					}
+			} else if c == -1 {
+				if node.Left != nil {
+					node = node.Left
 				} else {
-					if node.Right != nil {
-						node = node.Right
-					} else {
-						node.Right = &SetNode{Key: Key, Prev: node}
-						return true
-					}
+					node.Left = &SetNode{Key: Key, Prev: node}
+					return true
+				}
+			} else {
+				if node.Right != nil {
+					node = node.Right
+				} else {
+					node.Right = &SetNode{Key: Key, Prev: node}
+					return true
 				}
 			}
 		}
-
 	}
 
 	return false
