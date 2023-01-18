dccb2cf70d4ac0f11be0e1a63f6570890d56f28d
hejinxue <hejinxue001@163.com>
2023-01-18 15:47:50 +0800

    http context with cancel

--- internal/utils/async/async.go
+++ internal/utils/async/async.go
@@ -1,6 +1,7 @@
 package async
 
 import (
+	"context"
 	"net/http"
 	"time"
 )
@@ -17,6 +18,26 @@ func aHttp(url string) chan *HttpResponse {
 		client := http.Client{Timeout: 2 * time.Second}
 
 		res.Res, res.Err = client.Get(url)
+
+		httpRes <- res
+	}()
+
+	return httpRes
+}
+
+func aHttpCtx(ctx context.Context, url string) chan *HttpResponse {
+	httpRes := make(chan *HttpResponse)
+	go func() {
+		res := &HttpResponse{Res: &http.Response{}, Err: nil}
+		request, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
+		if err != nil {
+			res.Err = err
+			return
+		}
+
+		client := http.Client{Timeout: 2 * time.Second}
+		
+		res.Res, res.Err = client.Do(request)
 
 		httpRes <- res
 	}()
--- internal/utils/async/async_test.go
+++ internal/utils/async/async_test.go
@@ -1,16 +1,36 @@
 package async
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"testing"
+	"time"
 )
 
 func Test_aHttp(t *testing.T) {
-	resChan := aHttp("http://www.baidu.com")
+	resChan := aHttp("https://www.baidu.com")
 	defer close(resChan)
 
 	fmt.Println("feature mode")
+	res := <-resChan
+	if res.Err != nil {
+		log.Println(res.Err)
+	} else {
+		fmt.Println(res.Res.Status)
+	}
+}
+
+func Test_aHttpCtx(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	resChan := aHttpCtx(ctx, "https://www.google.com")
+	defer close(resChan)
+
+	fmt.Println("feature mode")
+	time.Sleep(time.Second)
+
+	cancel()
+
 	res := <-resChan
 	if res.Err != nil {
 		log.Println(res.Err)
